b03e11a0e99a2237e60b78dbe95fa95185e2d3c7
Karolina <karo129@wp.pl>
2023-05-03 20:51:17 +0200

    feature cart

--- app/pages/components/cart/cart.component.ts
+++ app/pages/components/cart/cart.component.ts
@@ -49,4 +49,12 @@ export class CartComponent implements OnInit {
     this.cartService.removeFromCart(item);
   }
 
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem) {
+    this.cartService.removeQuantity(item);
+  }
+
 }
