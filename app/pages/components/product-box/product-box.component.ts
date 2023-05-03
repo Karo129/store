1ec77dc241aec52a26bb3aeea7e526f63197044f
Karolina <karo129@wp.pl>
2023-05-03 17:39:06 +0200

    added product to cart

--- app/pages/components/product-box/product-box.component.ts
+++ app/pages/components/product-box/product-box.component.ts
@@ -1,16 +1,28 @@
-import { Component,Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { Product } from 'src/app/models/product.model';
 
 @Component({
-  selector: 'app-product-box',
-  templateUrl: './product-box.component.html',
-  styleUrls: ['./product-box.component.css']
+	selector: 'app-product-box',
+	templateUrl: './product-box.component.html',
+	styleUrls: ['./product-box.component.css'],
 })
 export class ProductBoxComponent implements OnInit {
-  @Input() fullWidthMode = false;
-  
-  constructor() { }
+	@Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snickers',
+    price: 150,
+    category: 'shoes',
+    description: 'Description',
+    image: 'https://via.placeholder/com/150'
+  };
+  @Output() addToCart = new EventEmitter();
 
-  ngOnInit(): void {
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	onAddToCart() {
+    this.addToCart.emit(this.product);
   }
-
 }
